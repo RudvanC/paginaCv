8d8170d84ccbc752880bf55938df25f1b335b1c1
RudvanC <rudvancastro@gmail.com>
2025-03-01 05:31:17 +0100

    casi finalizada

--- script.js
+++ script.js
@@ -2,7 +2,40 @@
 const hamburger = document.getElementById("hamburger");
 const navMenu = document.getElementById("nav-menu");
 
+
 // Agregar evento de clic al menú de hamburguesa
 hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active"); // Alternar la clase "active"
 });
+
+// Seleccionamos todos los enlaces dentro del menú
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => {
+        navMenu.classList.remove("active"); // Cierra el menú
+    });
+});
+
+
+document.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.navbar');
+  if (window.scrollY > 70) { 
+    navbar.classList.add('scrolled');
+  } else {
+    navbar.classList.remove('scrolled');
+  }
+});
+
+document.addEventListener('scroll', function () {
+  const nameNav = document.getElementById('name-nav');
+  if (window.scrollY > 70) { 
+    nameNav.classList.add('scrolled');
+  } else {
+    nameNav.classList.remove('scrolled');
+  }
+});
+
+
+
+
